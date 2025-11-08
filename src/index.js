13eb6d028db4a5d01fd6924744fe93bd05eddf3c
Nikhil Garg <nikhilgarg.drb@gmail.com>
2025-11-09 01:24:06 +0530

    setup basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-  
+const express = require("express");
+const { port } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+    const app = express();
+
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    });
+
+}
+
+setupAndStartServer();
