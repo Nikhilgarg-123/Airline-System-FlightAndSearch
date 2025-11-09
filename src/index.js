aa748041c03e0979f688440d541803f12f46ba21
Nikhil Garg <nikhilgarg.drb@gmail.com>
2025-11-09 15:29:45 +0530

    create DB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const { port } = require("./config/serverConfig");
+const { parse } = require("dotenv");
 
 const setupAndStartServer = async () => {
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     app.listen(port, () => {
         console.log(`Server started on port ${port}`);
