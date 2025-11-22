39b46b95647a9ee96226e6bb5c4e70d9dd6b56a4
Nikhil Garg <nikhilgarg.drb@gmail.com>
2025-11-22 17:38:01 +0530

    added repository logic for city repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,28 +1,46 @@
 const { City } = require('../models/index');
 
 class CityRepository {
-    async createCity(name) {
+    async createCity({name}) {
         try{
             const city = await City.create({ name });
             return city;
         } catch (error) {
+            console.log("Something went wrong at repositiory layer")
             throw error;
         }
-    }
-
-    async getAllCities() {
-        const cities = await City.findAll();
-        return cities;
     }
 
     async deleteCity(id) {
         try{
-            const city = await City.destroy({ where: { id } });
-            return city;
+            const city = await City.destroy({ where: { id: id } });
+            return true;
         } catch (error) {
+            console.log("Something went wrong at repositiory layer")
             throw error;
         }
     }
- }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+
+        }catch (error) {
+            console.log("Something went wrong at repositiory layer")
+            throw error;
+        }
+    }
+
+    async updateCity(id, name) {
+        try{
+            const city = await City.update({ name }, { where: { id: id } });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at repositiory layer")
+            throw error;
+        }
+    }
+}
 
 module.exports = new CityRepository();
