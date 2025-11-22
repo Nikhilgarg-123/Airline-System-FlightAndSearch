67ec37a8f694a44586a39dd3a153615c6fd09190
Nikhil Garg <nikhilgarg.drb@gmail.com>
2025-11-22 23:18:19 +0530

    added routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { port } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 const { parse } = require("dotenv");
 
 const setupAndStartServer = async () => {
@@ -9,6 +10,7 @@ const setupAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use("/api", ApiRoutes);
 
     app.listen(port, () => {
         console.log(`Server started on port ${port}`);
---
+++ src/routes/v1/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const CityController = require("../controllers/city-controller");
+
+const router = express.Router();
+
+router.post("/city", CityController.create);
+router.get("/city/:id", CityController.get);
+router.put("/city/:id", CityController.update);
+router.delete("/city/:id", CityController.destroy);
+
+module.exports = router;
